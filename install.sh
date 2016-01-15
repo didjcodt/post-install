df32d84fc05fd60f1610cadcdd8895813f286470
Lichard Torman <clement@decoodt.eu>
2016-01-15 01:11:00 +0100

    Adding install script, vimrc and tmux conf

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+clear
+cat << "EOF"
+
+ ,>   )\ `a_                                     _a' /(
+(  _  )/ /{_ ~~    Post installation script   ~~ _}\ \(  -
+ `(,)_,)/                                           \(,_(,\\
+  ,<_ ,<_.                                          ._>, _>,``==>
+
+EOF
+
+if [ "$(id -u)" != "0" ]
+then
+	echo "[!] Sorry, you must be root to use this post installation script"
+	exit 1
+fi
+
+echo "[+] Installing aptitude, ctags, tmux, tty-clock and vim"
+apt-get install aptitude ctags tmux tty-clock vim
+
+echo "[+] Downloading vimrc files"
+git clone https://github.com/didjcodt/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+echo "[+] Adding custom vimrc parameters"
+cp my_configs.vim ~/.vim_runtime/
+
+echo "[+] Adding .tmux.conf"
+cp .tmux.conf ~/
---
+++ my_configs.vim
@@ -0,0 +1,3 @@
+" Impose tabs and no spaces! 
+set noexpandtab
+
